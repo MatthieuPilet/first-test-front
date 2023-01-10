6255b687b209eed9ccef42adafdde9e1f3b5cbb9
DESKTOP-M56A960\matth <matthieu.pilet.dev@gmail.com>
2023-01-10 23:13:20 +0100

    [ADD] call function and show result

---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="header">
+            Test Header
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/component/TestCallFunctionComponent.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class UserComponent extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            users:{},
+        };
+    }
+
+     componentDidMount(){
+        fetch('http://localhost:8080/test/1')
+        .then((response) => response.json())
+        .then((data) => this.setState({users: data}));
+    }
+
+    render (){
+        const {users}= this.state;      
+        return (
+            <div>
+                <h1 className = "text-center"> Users List</h1>
+                <table className = "table table-striped">
+                    <thead>
+                        <tr>
+
+                            <td> User Id</td>
+                            <td> User First Name</td>
+                            <td> User Last Name</td>
+                            <td> User Email Id</td>
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                    { 
+                                <tr>
+                                    <td> {users.accountId}</td>   
+                                    <td> {users.accountUsername}</td>   
+                                    <td> {users.accountAge}</td>   
+                                    <td> {users.accountEmail}</td> 
+                                </tr>
+                            
+                        }
+                            
+
+                                
+
+                    </tbody>
+                </table>
+
+            </div>
+
+        )
+    }
+}
+
+export default UserComponent
