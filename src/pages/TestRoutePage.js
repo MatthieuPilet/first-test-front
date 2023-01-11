ab4d90c60a26a7859ec2b0870c57a7a4ebe7240a
DESKTOP-M56A960\matth <matthieu.pilet.dev@gmail.com>
2023-01-11 11:09:40 +0100

    [ADD]an other routing

--- src/pages/TestRoutePage.js
+++ src/pages/TestRoutePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 
   const TestRoutePage = () => {
     return (
       <>
         <nav>
           Cool tu as fais ton routage
+          <Link to="/">Test2</Link>
+                                    <Outlet />
         </nav>
       </>
     )
